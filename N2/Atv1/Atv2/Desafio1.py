a786337cbc09f03fd006864050fb637fecd78aa3
gustavobbarbosa4 <gustavobbarbosa4@gmail.com>
2026-04-29 21:13:43 -0300

    Pandas introd.

---
+++ N2/Atv1/Atv2/Desafio1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv",
+    sep=",",
+    encoding="utf-8",
+    na_values=["N/A","-"]
+)
+print("-------Desafio 1-------")
+print(df.head())
+print(df.info())
+print(df.shape)
+
+print("-------Desafio 2-------")
+print(df.describe())
+print(df["Pclass"].nunique())
+print(df["Sex"].value_counts())
+
+print("-------Desafio 3-------")
+print(df.loc[0:10, ["Name", "Age"]])
+print(" ")
+print(df.iloc[14:15, 0:11])
+
+print("-------Desafio 3-------")
+print(df[df["Age"] >= 60])
+minas_ricas = df[(df[Sex] == "female") &
+            (df["Pclass"] == 1)]
